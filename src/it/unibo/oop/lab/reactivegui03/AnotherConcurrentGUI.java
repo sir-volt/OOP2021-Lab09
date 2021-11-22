1069c92cd80b9ad48eae1ed12b265dc477ade1db
Andrea Bianchi - andrea.bianchi24@studio.unibo.it <andrea.bianchi24@studio.unibo.it>
2021-11-22 11:33:19 +0100

    added imports

--- src/it/unibo/oop/lab/reactivegui03/AnotherConcurrentGUI.java
+++ src/it/unibo/oop/lab/reactivegui03/AnotherConcurrentGUI.java
@@ -1,5 +1,20 @@
 package it.unibo.oop.lab.reactivegui03;
 
-public class AnotherConcurrentGUI {
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.lang.reflect.InvocationTargetException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+public class AnotherConcurrentGUI extends JFrame{
+    private static final long serialVersionUID = 1L;
+    
+    
 
 }
