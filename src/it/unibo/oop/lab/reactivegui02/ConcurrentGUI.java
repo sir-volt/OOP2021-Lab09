1d7c0793d2acc7abaabf5c2485de615e40343357
Andrea Bianchi - andrea.bianchi24@studio.unibo.it <andrea.bianchi24@studio.unibo.it>
2021-11-22 09:30:09 +0100

    created ConcurrentGUI.java following code from previous exercise. added up and down button each with their respective actionListener which change boolean value of isUp variable in Agent. modified Agent run to check isUp variable in order to properly increment or decrement

---
+++ src/it/unibo/oop/lab/reactivegui02/ConcurrentGUI.java
@@ -0,0 +1,131 @@
+package it.unibo.oop.lab.reactivegui02;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.lang.reflect.InvocationTargetException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+public class ConcurrentGUI extends JFrame{
+    
+    private static final double WIDTH_PERC = 0.2;
+    private static final double HEIGHT_PERC = 0.1;
+    private final JLabel display = new JLabel();
+    private final JButton up = new JButton("up");
+    private final JButton down = new JButton("down");
+    private final JButton stop = new JButton("stop");
+    
+    public ConcurrentGUI() {
+        final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setSize((int) (screenSize.getWidth() * WIDTH_PERC), (int) (screenSize.getHeight() * HEIGHT_PERC));
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        final JPanel panel = new JPanel();
+        panel.add(display);
+        panel.add(up);
+        panel.add(down);
+        panel.add(stop);
+        this.getContentPane().add(panel);
+        this.setVisible(true);
+        
+        final Agent agent = new Agent();
+        new Thread(agent).start();
+        
+        up.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                agent.goUp();
+            }  
+        });
+        
+        down.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                agent.goDown();
+            }     
+        });
+        
+        stop.addActionListener(new ActionListener() {
+            /**
+             * event handler associated to action event on button stop.
+             * 
+             * @param e
+             *            the action event that will be handled by this listener
+             */
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                // Agent should be final
+                agent.stopCounting();
+            }
+        });
+        
+    }
+    
+    private class Agent implements Runnable {
+
+        private boolean isUp = true;
+        private volatile boolean stop;
+        private volatile int counter;
+
+        @Override
+        public void run() {
+            while (!this.stop) {
+                try {
+                    /*
+                     * All the operations on the GUI must be performed by the
+                     * Event-Dispatch Thread (EDT)!
+                     */
+                    SwingUtilities.invokeAndWait(new Runnable() {
+                        @Override
+                        public void run() {
+                            // This will happen in the EDT: since i'm reading counter it needs to be volatile.
+                            ConcurrentGUI.this.display.setText(Integer.toString(Agent.this.counter));
+                        }
+                    });
+                    /*
+                     * SpotBugs shows a warning because the increment of a volatile variable is not atomic,
+                     * so the concurrent access is potentially not safe. In the specific case of this exercise,
+                     * we do synchronization with invokeAndWait, so it can be ignored.
+                     *
+                     * EXERCISE: Can you think of a solution that doesn't require counter to be volatile?
+                     */
+                    if (this.isUp) {
+                        this.counter++;
+                    } else {
+                        this.counter--;
+                    }
+                    
+                    Thread.sleep(100);
+                } catch (InvocationTargetException | InterruptedException ex) {
+                    /*
+                     * This is just a stack trace print, in a real program there
+                     * should be some logging and decent error reporting
+                     */
+                    ex.printStackTrace();
+                }
+            }
+        }
+
+        /**
+         * External command to stop counting.
+         */
+        public void stopCounting() {
+            this.stop = true;
+        }
+        
+        public void goUp() {
+            this.isUp = true;
+        }
+        
+        public void goDown() {
+            this.isUp = false;
+        }
+    }
+}
+
+
